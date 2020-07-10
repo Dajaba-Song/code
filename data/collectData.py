1626105a9531c247c44cf4fb57e6cd6bd353ee33
soo01imm <stellar1198@gmail.com>
2020-07-10 23:00:00 +0900

    initial settings for collecting data

---
+++ data/collectData.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+
+# options = webdriver.ChromeOptions()
+# options.add_argument("headless")
+# driver = webdriver.Chrome("./chromedriver", options=options)
+
+driver = webdriver.Chrome("./chromedriver")
